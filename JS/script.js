2d7784cd6462ecaf2d0831a57db187668c3f7ee9
Md Farhan Sajid <87274221+Farhan-2222@users.noreply.github.com>
2023-04-06 17:21:34 +0530

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -85,16 +85,40 @@ function showEase(){
 	document.getElementById('virtues-tab-2-content').style.display="none";
 }
 
-var swiper = new Swiper('.blog-slider', {
-      spaceBetween: 30,
-      effect: 'fade',
-      loop: true,
-      mousewheel: {
-        invert: false,
-      },
-      // autoHeight: true,
-      pagination: {
-        el: '.blog-slider__pagination',
-        clickable: true,
-      }
-    });
+const progressCircle = document.querySelector(".autoplay-progress svg");
+const progressContent = document.querySelector(".autoplay-progress span");
+var swiper = new Swiper(".mySwiper", {
+  
+
+    slidesPerView: 3,
+        // loop: true,
+        grabCursor : true,
+        spaceBetween: 20,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true
+        },
+        breakpoints: {
+          0: {
+            slidesPerView: 1,
+          },
+          550: {
+            slidesPerView: 1,
+          },
+          850: {
+            slidesPerView: 2,
+            // width:50,
+          },
+          1200: {
+            slidesPerView: 3,
+          },
+          1300: {
+            slidesPerView: 3,
+          },
+        },
+      });
+
